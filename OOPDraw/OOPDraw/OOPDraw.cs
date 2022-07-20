3bb8bcc03e165bceb3673ce59667e8de340194b6
pigeonjar273 <geraldivan273@gmail.com>
2022-07-20 23:27:14 +0100

    Added functionality to draw multiple lines

--- OOPDraw/OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw/OOPDraw.cs
@@ -1,3 +1,6 @@
+using System.Drawing;
+
+
 namespace OOPDraw
 {
     public partial class OOPDraw : Form
@@ -13,11 +16,15 @@ namespace OOPDraw
         bool dragging = false;
         Point startOfDrag = Point.Empty;
         Point lastMousePosition = Point.Empty;
+        List<Line> lines = new List<Line>();
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
-            gr.DrawLine(currentPen, startOfDrag, lastMousePosition);
+            foreach (Line line in lines)
+            {
+                line.Draw(gr);
+            }
 
         }
 
@@ -25,12 +32,15 @@ namespace OOPDraw
         {
             dragging = true;
             startOfDrag = lastMousePosition = e.Location;
+            lines.Add(new Line(currentPen, e.X, e.Y));
         }
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (dragging)
             {
+                Line currentLine = lines.Last();
+                currentLine.GrowTo(e.X, e.Y);
                 lastMousePosition = e.Location;
                 Refresh();
             }
@@ -41,4 +51,40 @@ namespace OOPDraw
             dragging = false;
         }
     }
+
+
+    public class Line
+    {
+        public Pen Pen { get; private set; }
+        public int X1 { get; private set; }
+        public int Y1 { get; private set; }
+        public int X2 { get; private set; }
+        public int Y2 { get; private set; }
+        public Line(Pen p, int x1, int y1, int x2, int y2)
+        {
+            Pen = p;
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
+        public Line(Pen p, int x1, int y1) : this(p, x1, y1, x1, y1)
+        {
+        }
+        public void Draw(Graphics g)
+        {
+            g.DrawLine(Pen, X1, Y1, X2, Y2);
+        }
+        public void GrowTo(int x2, int y2)
+        {
+            X2 = x2;
+            Y2 = y2;
+        }
+
+    }
+
+
+
+
+
 }
