c060ada00975bd01a5ec720f18e4186cfa7bdb70
pigeonjar273 <geraldivan273@gmail.com>
2022-07-20 22:00:00 +0100

    Fixed name

--- OOPDraw/OOPDraw/OOPDraw.Designer.cs
+++ OOPDraw/OOPDraw/OOPDraw.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // OOPDraw
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "OOPDraw";
+            this.Text = "OOPDraw";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
