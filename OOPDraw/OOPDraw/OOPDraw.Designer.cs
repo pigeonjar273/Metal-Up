ee868ed197637472fe05e5e3e9c30fe53906cb3f
pigeonjar273 <geraldivan273@gmail.com>
2022-07-20 22:48:54 +0100

    Added picture box

--- OOPDraw/OOPDraw/OOPDraw.Designer.cs
+++ OOPDraw/OOPDraw/OOPDraw.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.Canvas.Location = new System.Drawing.Point(252, 24);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(536, 414);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
             // 
             // OOPDraw
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Canvas);
             this.Name = "OOPDraw";
             this.Text = "OOPDraw";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox Canvas;
     }
 }
