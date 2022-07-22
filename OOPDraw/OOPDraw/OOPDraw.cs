42ce4f9ab50828182c287763e3f0d8c687c618c9
Michael Behrman <66805160+michaelbehrman@users.noreply.github.com>
2022-07-22 09:38:50 +0100

    github test

--- OOPDraw/OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw/OOPDraw.cs
@@ -90,6 +90,13 @@ namespace OOPDraw
             }
             currentPen = new Pen(color, currentPen.Width);
         }
+
+        //Test comment
+
+        private void OOPDraw_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 
 
