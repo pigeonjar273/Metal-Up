3ccd7f0a511e21a958b73629eac28b1801e04a8a
pigeonjar273 <geraldivan273@gmail.com>
2022-09-06 15:29:45 +0100

    Refactoring and tidying up

--- OOPDraw/OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw/OOPDraw.cs
@@ -4,9 +4,6 @@ using System.Drawing.Drawing2D;
 
 namespace OOPDraw
 {
-
-
-
     public partial class OOPDraw : Form
     {
 
@@ -277,7 +274,7 @@ namespace OOPDraw
             return (x, y, w, h);
         }
 
-        public void MoveBy(int xDelta, int yDelta)
+        public virtual void MoveBy(int xDelta, int yDelta)
         {
             X1 += xDelta;
             Y1 += yDelta;
@@ -299,6 +296,46 @@ namespace OOPDraw
 
     }
 
+    public class CompositeShape : Shape
+    {
+        private List<Shape> Components { get; set; }
+        public CompositeShape(List<Shape> components)
+        : base(new Pen(Color.Black, 1.0F), 0, 0, 0, 0)
+        {
+            Pen.DashStyle = DashStyle.Dash;
+            Components = components;
+            CalculateEnclosingRectangle();
+        }
+        private void CalculateEnclosingRectangle()
+        {
+            X1 = Components.Min(m => Math.Min(m.X1, m.X2));
+            Y1 = Components.Min(m => Math.Min(m.Y1, m.Y2));
+            X2 = Components.Max(m => Math.Max(m.X1, m.X2));
+            Y2 = Components.Max(m => Math.Max(m.Y1, m.Y2));
+        }
+
+        public override void Draw(Graphics g)
+        {
+            foreach (Shape m in Components)
+            {
+                m.Draw(g);
+            }
+            if (Selected) g.DrawRectangle(Pen, X1, Y1, X2 - X1, Y2 - Y1);
+        }
+        public override void MoveBy(int xDelta, int yDelta)
+        {
+            foreach (Shape m in Components)
+            {
+                m.MoveBy(xDelta, yDelta);
+            }
+            X1 += xDelta;
+            Y1 += yDelta;
+            X2 += xDelta;
+            Y2 += yDelta;
+        }
+
+
+    }
 
 
 
