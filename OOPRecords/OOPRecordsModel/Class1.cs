88843e67602f133353ab1112b7b73037be4d8c38
Michael Behrman <66805160+michaelbehrman@users.noreply.github.com>
2022-09-09 03:47:35 -0500

    Created OOPRecords

---
+++ OOPRecords/OOPRecordsModel/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace OOPRecordsModel
+{
+    public class Student
+    {
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age()
+        {
+            var today = DateTime.Today;
+            int ageToday = today.Year - DateOfBirth.Year;
+
+            if (today.Month < DateOfBirth.Month || (today.Month == DateOfBirth.Month && today.Day < DateOfBirth.Day))
+            {
+                ageToday--;
+            }
+
+            return ageToday;    
+
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, Age {Age()}";
+        }
+    }
+}
